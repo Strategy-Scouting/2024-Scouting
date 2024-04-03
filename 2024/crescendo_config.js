b24fa8111f531ab2583e0fde4223594767730964
Brenda Bytner <brenda.bytner@att.net>
2024-04-03 18:06:02 -0400

    change event to states

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2024 Troy #1",
+      "defaultValue": "2024 States",
       "required": "true"
     },
     { "name": "Match Level",
